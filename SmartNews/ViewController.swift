8440bf4b4fedd70c00b2d94a18ef8e1b9cd6655e
yasuo kurihara <breitling1216@gmail.com>
2017-04-03 00:34:11 +0900

    Initial Commit

---
+++ SmartNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartNews
+//
+//  Created by 栗原靖 on 2017/04/03.
+//  Copyright © 2017年 Yasuo Kurihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
